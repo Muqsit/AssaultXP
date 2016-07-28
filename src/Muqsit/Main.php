992096cd6a5ee85a3e90045fe4b5129c80eb62cd
Muqsit Rayyan <hackhack05@gmail.com>
2016-07-28 13:45:14 -0700

    Create Main.php

---
+++ src/Muqsit/Main.php
@@ -0,0 +1,36 @@
+<?php
+namespace Muqsit;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\entity\{EntityDamageEvent, EntityDamageByEntityEvent};
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener{
+
+  public function onEnable(){
+  	@mkdir($this->getDataFolder());
+  	$this->saveDefaultConfig();
+  	$this->reloadConfig();
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function rewardPlayer(EntityDamageEvent $e){
+    $p = $e->getEntity();
+    $cfg = $this->getConfig();
+    $rand = $cfg->get("chance");
+    $exp = $cfg->get("reward-xp");
+    $rewardMessage = $cfg->get("reward-message");
+    if($p instanceof Player && $e instanceof EntityDamageByEntityEvent){
+      $d = $e->getDamager();
+      switch(mt_rand(1, $rand)){
+        case 1:
+        $d->addExperience($exp);
+        $d->sendMessage($rewardMessage);
+        break;
+      }
+    }
+  }
+}
+      
+  
